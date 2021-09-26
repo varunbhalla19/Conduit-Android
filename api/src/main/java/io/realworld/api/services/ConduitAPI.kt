413479150f8250c5dae9bd1af03d7dc8c11f9a0d
Mac1 <varun@karmacircles.com>
2021-09-26 12:42:49 +0530

    added coroutines and modified Response type in services

--- api/src/main/java/io/realworld/api/services/ConduitAPI.kt
+++ api/src/main/java/io/realworld/api/services/ConduitAPI.kt
@@ -1,11 +1,20 @@
 package io.realworld.api.services
 
-import io.realworld.api.models.ArticlesResponse
-import retrofit2.Call
+import io.realworld.api.models.entities.SignupUser
+import io.realworld.api.models.entities.User
+import io.realworld.api.models.request.SignupRequest
+import io.realworld.api.models.response.ArticlesResponse
+import io.realworld.api.models.response.UserResponse
+import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface ConduitAPI {
 
     @GET("articles")
-    fun getArticles(): Call<ArticlesResponse>
+    suspend fun getArticles(
+    ): Response<ArticlesResponse>
+
 }
